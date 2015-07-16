8165bf30fd6d56400637aaa44f0cf7505c6f4456
GNR <GNR@afas.nl>
2015-07-16 23:20:02 +0200

    Basic Exception handling.

--- class.afasappconnector.php
+++ class.afasappconnector.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * This PHP class file will connect to an AFAS Software AppConnector.
+ * The connection is based on a One Time Password and a user-bound token.
+ * @author Gerard Nijboer
+ *
+ */
 class AFASappconnector {
 	// Configuration variable
 	private $config;
@@ -8,102 +14,101 @@ class AFASappconnector {
 		$this->config = $config;
 	}
 	
+	/**
+	 * Generate a One Time Password for the given userId.
+	 * The environmentKey is distilled from the global config file.
+	 * An OTP will be sent to the email address of the Profit user, if known.
+	 *
+	 * @param string $userId        	
+	 * @return multitype:NULL
+	 */
 	public function GenerateOTP($userId) {
-		$data = array(
-			'userId' => $userId,
-			'apiKey' => $this->config['apiKey'],
-			'environmentKey' => $this->config['environmentKey'],
-			'description' => $_SERVER['REMOTE_ADDR']
-		);
-		try {
-			$client = new SoapClient($this->config['webservices'].'tokenconnector.asmx?WSDL');
-			return $client->GenerateOTP($data);
-		} catch (\Exception $e) {
-			return array('error' => $e->getMessage());
-		}
-	}
-	
-	public function GetTokenFromOTP($userId, $otp) {
-		$data = array(
+		$data = array (
 				'userId' => $userId,
-				'apiKey' => $this->config['apiKey'],
-				'environmentKey' => $this->config['environmentKey'],
-				'otp' => $otp
+				'apiKey' => $this->config ['apiKey'],
+				'environmentKey' => $this->config ['environmentKey'],
+				'description' => $_SERVER ['REMOTE_ADDR'] 
 		);
 		try {
-			$client = new SoapClient($this->config['webservices'].'tokenconnector.asmx?WSDL');
-			return $client->GetTokenFromOTP($data);
-		} catch (\Exception $e) {
-			return array('error' => $e->getMessage());
+			$client = new SoapClient ( $this->config ['webservices'] . 'tokenconnector.asmx?WSDL' );
+			return $client->GenerateOTP ( $data );
+		} catch ( SoapFault $e ) {
+			throw $e;
 		}
 	}
-	
+	public function GetTokenFromOTP($userId, $otp) {
+		$data = array (
+				'userId' => $userId,
+				'apiKey' => $this->config ['apiKey'],
+				'environmentKey' => $this->config ['environmentKey'],
+				'otp' => $otp 
+		);
+		try {
+			$client = new SoapClient ( $this->config ['webservices'] . 'tokenconnector.asmx?WSDL' );
+			return $client->GetTokenFromOTP ( $data );
+		} catch ( SoapFault $e ) {
+			throw $e;
+		}
+	}
 	public function GetData($token, $connectorId, $filtersXml, $skip = 0, $take = 10) {
 		// Prepare the data to send with the postback
 		$data = array (
-				'token' => '<token><version>1</version><data>'.$token.'</data></token>',
+				'token' => '<token><version>1</version><data>' . $token . '</data></token>',
 				'connectorId' => $connectorId,
 				'filtersXml' => $filtersXml,
 				'skip' => $skip,
-				'take' => $take
+				'take' => $take 
 		);
 		
 		try {
-			$client = new SoapClient($this->config['webservices'].'appconnectorget.asmx?WSDL');
-			return simplexml_load_string($client->GetData($data)->GetDataResult);
-		} catch (\Exception $e) {
-			return array('error' => $e->getMessage());
+			$client = new SoapClient ( $this->config ['webservices'] . 'appconnectorget.asmx?WSDL' );
+			return simplexml_load_string ( $client->GetData ( $data )->GetDataResult );
+		} catch ( SoapFault $e ) {
+			throw $e;
 		}
 	}
-	
 	public function Execute($token, $connectorType, $dataXml, $connectorVersion = 1) {
-		$data = array(
-			'token' => '<token><version>1</version><data>'.$token.'</data></token>',
-			'connectorType' => $connectorType,
-			'connectorVersion' => $connectorVersion,
-			'dataXml' => $dataXml
+		$data = array (
+				'token' => '<token><version>1</version><data>' . $token . '</data></token>',
+				'connectorType' => $connectorType,
+				'connectorVersion' => $connectorVersion,
+				'dataXml' => $dataXml 
 		);
 		try {
-			$client = new SoapClient($this->config['webservices'].'appconnectorupdate.asmx?WSDL');
-			return $client->Execute($data);
-		} catch (\Exception $e) {
-			return array('error' => $e->getMessage());
+			$client = new SoapClient ( $this->config ['webservices'] . 'appconnectorupdate.asmx?WSDL' );
+			return $client->Execute ( $data );
+		} catch ( SoapFault $e ) {
+			throw $e;
 		}
 	}
-	
 	public function DeleteToken($token) {
-		$data = array(
-				'token' => '<token><version>1</version><data>'.$token.'</data></token>'
+		$data = array (
+				'token' => '<token><version>1</version><data>' . $token . '</data></token>' 
 		);
 		try {
-			$client = new SoapClient($this->config['webservices'].'tokenconnector.asmx?WSDL');
-			return $client->DeleteToken($data);
-		} catch (\Exception $e) {
-			return array('error' => $e->getMessage());
+			$client = new SoapClient ( $this->config ['webservices'] . 'tokenconnector.asmx?WSDL' );
+			return $client->DeleteToken ( $data );
+		} catch ( SoapFault $e ) {
+			throw $e;
 		}
 	}
-	
 }
-
 class AFASfilterxml {
 	private $filters;
-	
 	public function AFASfilterxml() {
-		$this->filters = array();
+		$this->filters = array ();
 	}
-	
 	public function addFilter(array $fields) {
-		$result = '<Filter FilterId="'.rand(0, time()).'">';
-		foreach ($fields as $field) {
-			$result .= '<Field OperatorType="'.$field->OperatorType.'" FieldId="'.$line->FieldId.'">'.$line->Value.'</Field>';
+		$result = '<Filter FilterId="' . rand ( 0, time () ) . '">';
+		foreach ( $fields as $field ) {
+			$result .= '<Field OperatorType="' . $field->OperatorType . '" FieldId="' . $line->FieldId . '">' . $line->Value . '</Field>';
 		}
 		$result .= '</Filter>';
-		$this->filters[] = $result;
+		$this->filters [] = $result;
 	}
-	
 	public function getFilterXml() {
 		$result = '<Filters>';
-		foreach ($this->filters as $filter) {
+		foreach ( $this->filters as $filter ) {
 			$result .= $filter;
 		}
 		$result .= '</Filters';
